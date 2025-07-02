88b263ea5d8fd7d693ab8b8702e5a2a51d2fc66c
luizznelson <luizznelson.lima@gmail.com>
2025-07-02 13:03:30 -0300

    migração alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Garante que a pasta raiz (onde está app/) esteja no PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
